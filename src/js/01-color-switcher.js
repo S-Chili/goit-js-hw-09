fd7a405b474fd3456afaa7510afdbbb38ae88749
Anastasiia <schili@outlook.com>
2023-02-20 16:06:08 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+});
