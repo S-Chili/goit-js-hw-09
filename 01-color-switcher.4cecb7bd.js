951359b3504db5bdf8f31732ddb60e5629a70458
S-Chili <118385394+S-Chili@users.noreply.github.com>
2023-02-20 14:06:35 +0000

    Deploying to gh-pages from @ S-Chili/goit-js-hw-09@fd7a405b474fd3456afaa7510afdbbb38ae88749 🚀

---
+++ 01-color-switcher.4cecb7bd.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3),n.disabled=!1})),n.addEventListener("click",(function(){n.disabled=!0,clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.4cecb7bd.js.map
---
+++ 01-color-switcher.c5de6750.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d;t.addEventListener("click",(()=>{t.disabled=!0,d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3),e.disabled=!1})),e.addEventListener("click",(()=>{e.disabled=!0,clearInterval(d),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.c5de6750.js.map
